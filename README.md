040b2fed7103d534bfc591dd017ac51b90f07741
Alexandre Pellereau <alexandre.pellereau@epita.fr>
2022-12-31 20:19:52 +0100

    Set-up

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # What is this mod for ?
 
 It's just my first mod to learn Minecraft modding
+
+url of the tutorial: https://www.youtube.com/watch?v=RSqSZoJQXvg&list=PLKGarocXCE1EeLZggaXPJaARxnAbUD8Y_&index=1
---
+++ src/main/java/net/dralexgon/learnmodding/LearnModdingClient.java
@@ -0,0 +1,10 @@
+package net.dralexgon.learnmodding;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearnModdingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This code runs when Minecraft is in the client.
+    }
+}
